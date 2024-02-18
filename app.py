bc4fe24cd9fa0f9f92730efeda34e9d82eaf7dd1
Zeinab <zeinab@Zeinabs-MacBook-Air.local>
2024-02-18 19:33:48 +0100

    another tree tested

--- app.py
+++ app.py
@@ -9,7 +9,18 @@ graph1 = {
     "F": ['E'],
 }
 
-dfs = DFS(graph=graph1)
+graph2 = {
+    "0": ["1", "2"],
+    "1": ["0", "3"],
+    "2": ["0", "1", "3"],
+    "3": ["1", "2"],
+    "4": ["5", "6"],
+    "5": ["4", "6"],
+    "6": ["4", "5", "7"],
+    "7": ["6"],
+}
+
+dfs = DFS(graph=graph2)
 forest = dfs.complete_dfs()
 for tree in forest:
     print(tree)
